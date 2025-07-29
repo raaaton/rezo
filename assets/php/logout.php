f1ad3fa95a54fd49b3b82431196a8981aed8014f
raaaton <pranolouis@gmail.com>
2025-07-29 23:50:18 +0200

    small fixes

--- assets/php/logout.php
+++ assets/php/logout.php
@@ -1,11 +1,10 @@
 <?php
-require '../includes/db.php';
+require __DIR__ . '/../../includes/db.php';
 
 if (isset($_COOKIE['auth_token'])) {
     $stmt = $pdo->prepare("UPDATE users SET auth_token = NULL, token_expiry = NULL WHERE auth_token = ?");
     $stmt->execute([$_COOKIE['auth_token']]);
 
-    // Supprime le cookie
     setcookie('auth_token', '', time() - 3600, '/');
 }
 
